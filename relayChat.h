c44b94b3e08d27fcffd69dbb115ee5cb74545a39
Micah Lorenz <milorenz@pdx.edu>
2023-11-27 22:18:19 -0800

    common.c added, skeleton of client

--- relayChat.h
+++ relayChat.h
@@ -9,36 +9,41 @@
 #include <stdint.h>
 #include <errno.h>
 #include <pthread.h>
-#include <sys/socket.h>//commented out because I still haven't managed to convince VSCode to us the linux versions of things.
+#include <sys/types.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
 
 //OPCODES
-#define IRC_OPCODE_ERR				= 0x10000001
-#define IRC_OPCODE_HEARTBEAT		= 0x10000002
-#define IRC_OPCODE_HELLO			= 0x10000003
-#define IRC_OPCODE_LIST_ROOMS		= 0x10000004
-#define IRC_OPCODE_LIST_ROOMS_RESP	= 0x10000005	
-#define IRC_OPCODE_LIST_USERS_RESP	= 0x10000006
-#define IRC_OPCODE_JOIN_ROOM		= 0x10000007
-#define IRC_OPCODE_LEAVE_ROOM		= 0x10000008
-#define IRC_OPCODE_SEND_MSG			= 0x10000009
-#define IRC_OPCODE_TELL_MSG			= 0x1000000A
-#define IRC_OPCODE_SEND_PRIV_MSG	= 0x1000000B
-#define IRC_OPCODE_TELL_PRIV_MSG	= 0x1000000C
-#define IRC_OPCODE_KEY_EXCHANGE		= 0x1000000D
+#define IRC_OPCODE_ERR				0x10000001
+#define IRC_OPCODE_HEARTBEAT		0x10000002
+#define IRC_OPCODE_HELLO			0x10000003
+#define IRC_OPCODE_LIST_ROOMS		0x10000004
+#define IRC_OPCODE_LIST_ROOMS_RESP	0x10000005	
+#define IRC_OPCODE_LIST_USERS_RESP	0x10000006
+#define IRC_OPCODE_JOIN_ROOM		0x10000007
+#define IRC_OPCODE_LEAVE_ROOM		0x10000008
+#define IRC_OPCODE_SEND_MSG			0x10000009
+#define IRC_OPCODE_TELL_MSG			0x1000000A
+#define IRC_OPCODE_SEND_PRIV_MSG	0x1000000B
+#define IRC_OPCODE_TELL_PRIV_MSG	0x1000000C
+#define IRC_OPCODE_KEY_EXCHANGE		0x1000000D
 
 
 //ERROR CODES
-#define IRC_ERR_UNKNOWN			= 0x20000001
-#define IRC_ERR_ILLEGAL_OPCODE	= 0x20000002
-#define IRC_ERR_ILLEGAL_LENGTH	= 0x20000003
-#define IRC_ERR_NAME_EXISTS		= 0x20000004
-#define IRC_ERR_ILLEGAL_NAME	= 0x20000005
-#define IRC_ERR_ILLEGAL_MESSAGE	= 0x20000006
-#define IRC_ERR_TOO_MANY_USERS	= 0x20000007
-#define IRC_ERR_TOO_MANY_ROOMS	= 0x20000008
-#define IRC_ERR_WRONG_VERSION	= 0x20000009
-#define IRC_ERR_NO_HEARTBEAT	= 0x2000000A
+#define IRC_ERR_UNKNOWN			0x20000001
+#define IRC_ERR_ILLEGAL_OPCODE	0x20000002
+#define IRC_ERR_ILLEGAL_LENGTH	0x20000003
+#define IRC_ERR_NAME_EXISTS		0x20000004
+#define IRC_ERR_ILLEGAL_NAME	0x20000005
+#define IRC_ERR_ILLEGAL_MESSAGE	0x20000006
+#define IRC_ERR_TOO_MANY_USERS	0x20000007
+#define IRC_ERR_TOO_MANY_ROOMS	0x20000008
+#define IRC_ERR_WRONG_VERSION	0x20000009
+#define IRC_ERR_NO_HEARTBEAT	0x2000000A
 
+#define MAXMSGLENGTH 8000
 
 //Generic Messages
 struct irc_packet_header {
@@ -50,7 +55,7 @@ struct irc_packet_header {
 //for the other packets defined below
 struct irc_packet_generic {
 	struct irc_packet_header header;
-	uint8_t content[]//header.length];
+	uint8_t content[];//header.length];
 };
 
 // Heartbeat Messages
@@ -100,7 +105,7 @@ struct irc_packet_send_msg {
 	struct irc_packet_header header;// = 
 		//{.opcode = IRC_OPCODE_SEND_MSG, .length = LENGTH};
 	char target_name[20];
-	char msg[]//LENGTH - 20];
+	char msg[];//LENGTH - 20];
 };
 
 // SERVER MESSAGES
@@ -110,7 +115,7 @@ struct irc_packet_list_resp {
 	struct irc_packet_header header;// = 
 		//{.opcode = IRC_OPCODE_LIST_ROOMS_RESP, .length = LENGTH};
 	char identifier[20];
-	char item_names[]//(LENGTH/20) - 1][20];
+	char item_names[];//(LENGTH/20) - 1][20];
 };
 
 // User Message Forwarding
@@ -119,8 +124,11 @@ struct irc_packet_tell_msg {
 		//{.opcode = IRC_OPCODE_TELL_MSG, .length = LENGTH};
 	char target_name[20];
 	char sending_user[20];
-	char msg[]//LENGTH - 40];
+	char msg[];//LENGTH - 40];
 };
+
+int validate_string(char * text, int length);
+
 
 
 #endif
--- relayChatClient.c
+++ relayChatClient.c
@@ -6,15 +6,97 @@
 #include "relayChat.h" 
 #include <string.h>
 
+void keepalive();
+void receiver();
+
+
+pthread_mutex_t timestamp_mutex;
+time_t lastSent;
+time_t lastRecvd;
+pthread_mutex_t conMTX;
+int connected = 1;
+char username[20] = "";
+
+
+void keepalive()
+{
+    time_t now = time(NULL);
+    int connected = 1;
+    while(1)
+    {
+        pthread_mutex_lock(&timestamp_mutex);
+        if(now - lastRecvd >= 15)
+        {//server is not sending heartbeats, assume connection lost
+            //Attempt to reconnect?
+            pthread_mutex_lock(&conMTX);
+
+
+            pthread_mutex_unlock(&conMTX);
+        }
+        if(now - lastSent >= 3)
+        {//Outgoing heartbeat is stale
+            //send heartbeat
+        }
+        pthread_mutex_unlock(&timestamp_mutex);
+        sleep(3);
+    }
+    return;
+}
+
+void receiver()
+{
+    void * buf = malloc(sizeof(struct irc_packet_tell_msg) + MAXMSGLENGTH * sizeof(char));
+    pthread_mutex_lock(&conMTX);
+    while(connected);
+    {
+        pthread_mutex_unlock(&conMTX);
+
+        //recv(sock, );
+        
+        pthread_mutex_lock(&timestamp_mutex);
+        lastRecvd = time(NULL);
+        pthread_mutex_unlock(&timestamp_mutex);
+        struct irc_packet_generic * input = buf;
+        switch (input->header.opcode)
+        {
+        case IRC_OPCODE_ERR:
+            /* code */
+            break;
+        case IRC_OPCODE_HEARTBEAT:
+            //no work necessary, timestamp already updated
+            break;
+        case IRC_OPCODE_LIST_ROOMS_RESP:
+
+            break;
+        case IRC_OPCODE_LIST_USERS_RESP:
+            break;
+        case IRC_OPCODE_TELL_MSG:
+            break;
+        case IRC_OPCODE_TELL_PRIV_MSG:
+            break;
+        default:
+            //invalid opcode
+
+            break;
+        }
+
+
+        pthread_mutex_lock(&conMTX);
+    }
+}
+
 int main(int argc, char *argv[]) {
 
-    char username[20];
+    lastRecvd = time(NULL);
+    lastSent = time(NULL);
 
-    struct irc_packet_send_msg * test = malloc(2 * sizeof(struct irc_packet_header) + 20 * sizeof(char) + 27 * sizeof(char));
+    struct irc_packet_send_msg * test = malloc( sizeof(struct irc_packet_send_msg) + 27 * sizeof(char));
 
     strcpy(test->msg, "abcdefghijklmnopqrstuwxyz");
 
     printf("%s\n", test->msg);
+
+
 
 
     exit(0);
